31fb0111e587e8b21a1b9b47de341f0de5bd2eeb
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2024-09-25 09:18:54 -0700

    fix: add todo stuff

--- javascript/ipc.js
+++ javascript/ipc.js
@@ -1,1 +1,21 @@
-console.log('Hello from crowser');
+window.__CROWSER = {}
+
+/**
+ * IPC related functionality, such as events and sending messages
+ */
+window.__CROWSER.ipc = {
+  send: (data) => {
+    // TODO
+  },
+  event: {
+    listen: (evt, callback) => {
+      // TODO
+    },
+    unlisten: (evt, callback) => {
+      // TODO
+    },
+    emit: (evt, payload) => {
+      // TODO
+    }
+  }
+}
